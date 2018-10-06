e82c2d39bfe5ea988117524df7456e2eccf555c3
niharika <41866167+SachdevaSaab@users.noreply.github.com>
2018-10-06 13:39:04 -0400

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 22:11:59 2018
+
+@author: hello
+"""
+
+import os
+import csv
+from datetime import datetime
+budgetdataCSV = os.path.join('C:/Users/hello/Desktop/python-challenge/PyBank/budget_data.csv')
+with open(budgetdataCSV, 'r') as csvfile:
+    
+    # Split the data on commaas
+    reader = csv.reader(csvfile, delimiter=',')
+    next(reader)
+    row_count = sum(1 for r in reader)    
+    print("Total Months: " + str(row_count))
+        
+    csvfile.seek(0)
+    next(reader)
+      
+    csvfile.seek(0)
+    next(reader)
+    
+    avgdifflist = []
+    prev = 0
+    profit_loss = 0   
+    for x in reader:
+        profit_loss = profit_loss + int(x[1])
+        avgdiff =int(x[1])-prev 
+        prev = int(x[1])
+        avgdifflist.append(avgdiff)
+        length  = len(avgdifflist)-1
+        
+    total_avg = sum(avgdifflist[1:]) / length
+        
+    print("Total: $" +str(profit_loss))
+    print("Average Change: $" +str(total_avg))
+    print("Greatest Increase in Profits: (" + str(max(avgdifflist)) + ")")
+    print("Greatest Decrease in Profits: (" + str(min(avgdifflist)) + ")")
+    
+   
